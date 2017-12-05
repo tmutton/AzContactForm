352ab1ccd0cc4873195a26d36e48524a83392697
SNRDEV2\tmutt <tmutton@spacehive.com>
2017-12-05 18:21:28 +0000

    experiment

--- AzContactForm.FunctionApp/PostMessage.cs
+++ AzContactForm.FunctionApp/PostMessage.cs
@@ -6,14 +6,27 @@ using Microsoft.Azure.WebJobs.Host;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
 namespace AzContactForm
 {
     public static class PostMessage
     {
+        public const string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=ztepb7oskerymstorage;AccountKey=TpIjQT0finPgswlPEFaxixvfRShszopN3SlAx3eNJ+TVgIMd066rIllvIKimYIPECieRfVi08lzOWnscJQ5Uyg==;EndpointSuffix=core.windows.net";
+        public const string TableName = "test";
+        
         [FunctionName("Post")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "message")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
+
+            // Create account, client and table
+            var account = CloudStorageAccount.Parse(ConnectionString);
+            var tableClient = account.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(TableName);
+            await table.CreateIfNotExistsAsync();
 
             // parse query parameters
             log.Info("Parsing query parameters");
@@ -41,12 +54,27 @@ namespace AzContactForm
 
             // todo: add validation for parameters
 
+            // Insert new value in table
+            var coinBtc = new ContactEntity
+            {
+                Symbol = "abc",
+                RowKey = "rowBtc" + DateTime.Now.Ticks,
+                PartitionKey = "partition",
+            };
+
+            table.Execute(TableOperation.Insert(coinBtc));
+
             // create the message object
             // todo: do something with this
             //var contactMessage = new ContactMessage(name, email, message);
-            
+
             // return a 200 response message
             return req.CreateResponse(HttpStatusCode.OK, "Thank you for your message");
         }
+    }
+
+    public class ContactEntity : TableEntity
+    {
+        public string Symbol { get; set; }
     }
 }
