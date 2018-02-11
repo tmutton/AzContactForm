ab818339d38ade463957d93d79c4f80d419b9456
Thomas Mutton <tmutton1@gmail.com>
2018-02-11 22:16:31 +0000

    PostMessage now sends an event to EventGrid

---
+++ AzContactForm.FunctionApp/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace AzContactForm.FunctionApp
+{
+    //https://docs.microsoft.com/en-us/azure/event-grid/event-schema
+
+    public class Event<T>
+    {
+        public string Topic { get; set; }
+
+        public string Subject { get; set; }
+
+        public string EventType { get; set; }
+
+        public string EventTime { get; set; }
+
+        public string Id { get; set; }
+
+        public T Data { get; set; }
+    }
+}
--- AzContactForm.FunctionApp/PostMessage.cs
+++ AzContactForm.FunctionApp/PostMessage.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using AzContactForm.FunctionApp;
 using FluentValidation.Results;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace AzContactForm
 {
     public static class PostMessage
     {
+        public static string eventTopic = "/SUBSCRIPTIONS/EFCF1B1C-B797-4AD3-B432-43D90045E26D/RESOURCEGROUPS/FIBTO/PROVIDERS/MICROSOFT.EVENTGRID/TOPICS/DELIVERYREQUESTS";
+
+        public static string eventTopicUrl = "https://deliveryrequests.westus2-1.eventgrid.azure.net/api/events";
+
+        public static string eventTopicSasKey = "kv2rlUCX34Qx933/6ijZSiNwnjB1LzovggSeWy1RhFM=";
+
         [FunctionName("Post")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "message")]HttpRequestMessage req, TraceWriter log)
         {
@@ -55,6 +65,38 @@ namespace AzContactForm
             if (!results.IsValid) return req.CreateResponse(HttpStatusCode.BadRequest, validationResponse);
 
             // todo: send event to event grid
+            var events = new List<Event<Message>>();
+
+            var event1 = new Event<Message>
+            {
+                Id = Guid.NewGuid().ToString(),
+                Topic = eventTopic,
+                Subject = "WindDetails",
+                EventTime = DateTimeOffset.Now.ToString("o"),
+                EventType = "newRequest",
+                Data = messageToSave
+            };
+            events.Add(event1);
+
+            HttpClient httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(eventTopicUrl)
+            };
+
+            var json = JsonConvert.SerializeObject(events);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
+            request.Headers.Add("aeg-sas-key", eventTopicSasKey);
+            request.Content = new StringContent(json,
+                                                Encoding.UTF8,
+                                                "application/json");
+
+            httpClient.SendAsync(request)
+                  .ContinueWith(responseTask =>
+                  {
+                      Console.WriteLine("Response: {0}", responseTask.Result);
+                  });
+
             log.Info("Sent Event to Event Grid");
 
             // return a 200 response message
